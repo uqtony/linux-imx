eae73ad06ce884ba40fc94ef6fd01f6053bbb77c
uqtony <uqtony@gmail.com>
2025-07-09 05:26:29 +0800

    [EIDEAL][DISPLAY]Configure Lontium9211c MIPI to be NO_HFP
    1. To solve 1080p60fps not work issue, configure Lontium9211c as
    NO_HFP, NO_HBP and NO_HSA which cause invalid line-pattern.
    2. Line-pattern format is RGB, but actually is BRG.
    3. Configure MIPI to be NO_HFP, can also solve 1080p60fps not work issue,
    and avoid invalid line-pattern.

--- drivers/gpu/drm/bridge/lontium-lt9211c-eideal.c
+++ drivers/gpu/drm/bridge/lontium-lt9211c-eideal.c
@@ -1600,7 +1600,7 @@ static int lt9211c_attach_dsi(struct lt9211c *ctx)
 	ctx->dsi = dsi;
 	dsi->lanes = 4;
         dsi->format = MIPI_DSI_FMT_RGB888;
-        dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST | MIPI_DSI_MODE_VIDEO_NO_HFP | MIPI_DSI_MODE_VIDEO_NO_HBP | MIPI_DSI_MODE_VIDEO_NO_HSA;
+        dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST | MIPI_DSI_MODE_VIDEO_NO_HFP;
 	ret = mipi_dsi_attach(dsi);
         if (ret < 0) {
                 dev_err(dev, "failed to attach dsi to host\n");
